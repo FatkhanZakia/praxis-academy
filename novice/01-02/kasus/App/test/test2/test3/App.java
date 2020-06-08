66418a815f55099f543c4e1ebce5b36b8a7ca72c
FatkhanZakia <zakiafattan@.com>
2020-06-08 11:40:39 +0700

    upload latihan dan kasus

---
+++ novice/01-02/kasus/App/test/test2/test3/App.java
@@ -0,0 +1,11 @@
+package test2.test3;
+
+public class App {
+    public String getGreeting(){
+        return "Hello Fatan";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+    }
+}
