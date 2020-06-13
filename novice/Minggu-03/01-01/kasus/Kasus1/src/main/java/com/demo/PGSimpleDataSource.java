daa36d025cc486edfeb479585d9159d3b0849452
FatkhanZakia <zakiafattan@.com>
2020-06-13 19:52:48 +0700

    commit

---
+++ novice/Minggu-03/01-01/kasus/Kasus1/src/main/java/com/demo/PGSimpleDataSource.java
@@ -0,0 +1,33 @@
+package com.demo;
+
+public class PGSimpleDataSource {
+
+    public void setServerName(String roach1) {
+
+    }
+
+    public void setPortNumber(int i) {
+
+    }
+
+    public void setDatabaseName(String bank) {
+
+    }
+
+    public void setUser(String maxroach) {
+
+    }
+
+    public void setPassword(Object o) {
+
+    }
+
+    public void setReWriteBatchedInserts(boolean b) {
+
+
+    }
+
+    public void setApplicationName(String basicDemo) {
+
+    }
+}
